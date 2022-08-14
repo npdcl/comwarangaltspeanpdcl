5646c678988c169a8aa90c497b30102c66bb7a79
vinodkumarjakkanagari <vinod.jakkanagari@gmail.com>
2022-08-14 11:28:10 +0530

    Add project files.

---
+++ Controllers/FileController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TspeaNpdcl3.Models;
+
+namespace TspeaNpdcl3.Controllers
+{
+    public class FileController : Controller
+    {
+        IWebHostEnvironment _webHostEnvironment = null;
+
+        public FileController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+
+        }
+
+        [HttpGet]
+        public IActionResult Index(string fileName = "")
+        {
+            FileClass fileObj = new FileClass();
+            fileObj.name = fileName;
+            string path = $"{_webHostEnvironment.WebRootPath}\\files\\";
+            int nid = 1;
+            foreach (string pdfPath in Directory.EnumerateFiles(path, "*.pdf"))
+            {
+                fileObj.file.Add(new FileClass()
+                {
+                    Fileid = nid++,
+                    name = Path.GetFileName(pdfPath),
+                    path = pdfPath
+                });
+
+            }
+            return View(fileObj);
+
+        }
+        [HttpPost]
+        public IActionResult Index(IFormFile file, [FromServices] IWebHostEnvironment webHostEnvironment)
+        {
+            string fileName = $"{webHostEnvironment.WebRootPath}\\files\\{file.FileName}";
+            using (FileStream fileStream = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+
+            }
+                return Index();
+
+        }
+        public IActionResult PDFviewNewTab (string fileName)
+        {
+            string path = _webHostEnvironment.WebRootPath+"\\files\\"+ fileName;
+
+            return File(System.IO.File.ReadAllBytes(path),"application/pdf");
+
+
+        }
+    }
+}
---
+++ Models/FileClass.cs
@@ -0,0 +1,12 @@
+﻿namespace TspeaNpdcl3.Models
+{
+    public class FileClass
+    {
+        public int Fileid { get; set; } = 0;
+        public string name { get; set; } = "";
+        public string path { get; set; } = "";
+
+        public List<FileClass> file { get; set; } = new List<FileClass>();   
+
+    }
+}
